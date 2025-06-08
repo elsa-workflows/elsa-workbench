875cf3c075aa89d2996fc3c6bf6f5ce40a9a3fd6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-06-08 21:31:34 +0200

    Adds initial Elsa Workbench core web project
    Creates the initial Elsa Workbench core web project with necessary project files, configurations, and dependencies.
    
    This commit introduces the core web project for Elsa Workbench, including:
    
    - Project file (.csproj) with references to Elsa core and extension modules
    - appsettings.json with configuration settings for various databases, multitenancy, identity, HTTP, scripting, and other features
    - Enums for ApplicationRole, DistributedCachingTransport, MassTransitBroker, PersistenceProvider, SqlDatabaseProvider, and WorkflowRuntime
    - Extensions for multitenant connection string retrieval
    - Filters for masking sensitive HTTP request headers
    - FodyWeavers.xml for configuring Fody
    - Program.cs for configuring and running the application
    - Properties/launchSettings.json for configuring the launch settings of the application
    - README.md with basic information about the server project.

---
+++ src/workbench/Elsa.Server.Core.Web/Enums/SqlDatabaseProvider.cs
@@ -0,0 +1,13 @@
+namespace Elsa.Server.Core.Web;
+
+public enum SqlDatabaseProvider
+{
+    SqlServer,
+    Sqlite,
+    MySql,
+    PostgreSql,
+    Citus,
+    YugabyteDb,
+    Oracle,
+    CockroachDb
+}
---
+++ src/workbench/Elsa.Server.Core.Web/Enums/WorkflowRuntime.cs
@@ -0,0 +1,8 @@
+namespace Elsa.Server.Core.Web;
+
+public enum WorkflowRuntime
+{
+    Default,
+    Distributed,
+    ProtoActor
+}
