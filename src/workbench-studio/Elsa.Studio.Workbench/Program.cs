0af0cb6b0347f8f67ea03ed914caf22d8c05f30f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-06-24 14:02:44 +0200

    Initial setup for Elsa Studio Workbench project

---
+++ src/workbench-studio/Elsa.Studio.Workbench/Program.cs
@@ -0,0 +1,92 @@
+using Elsa.Studio.Core.BlazorServer.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
+using Elsa.Studio.Extensions;
+using Elsa.Studio.Localization.Time;
+using Elsa.Studio.Localization.Time.Providers;
+using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Models;
+using Elsa.Studio.Shell.Extensions;
+using Elsa.Studio.Workflows.Extensions;
+using Elsa.Studio.Workflows.Designer.Extensions;
+using Elsa.Studio.Localization.BlazorServer.Extensions;
+using Elsa.Studio.Localization.Models;
+using Elsa.Studio.Localization.Options;
+using Elsa.Studio.Translations;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Elsa.Studio.Branding;
+using Elsa.Studio.Host.Server;
+using Elsa.Studio.Login.Extensions;
+
+// Build the host.
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Register Razor services.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.RootComponents.RegisterCustomElsaStudioElements();
+    options.RootComponents.MaxJSRootComponents = 1000;
+});
+
+// Register shell services and modules.
+var backendApiConfig = new BackendApiConfig
+{
+    ConfigureBackendOptions = options => configuration.GetSection("Backend").Bind(options),
+    ConfigureHttpClientBuilder = options =>
+    {
+        options.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler);
+        options.ConfigureHttpClient = (_, client) =>
+        {
+            // Set a long time out to simplify debugging both Elsa Studio and the Elsa Server backend.
+            client.Timeout = TimeSpan.FromHours(1);
+        };
+    },
+};
+
+var localizationConfig = new LocalizationConfig
+{
+    ConfigureLocalizationOptions = options =>
+    {
+        configuration.GetSection(LocalizationOptions.LocalizationSection).Bind(options);
+        options.SupportedCultures = new[] { options?.DefaultCulture ?? new LocalizationOptions().DefaultCulture }
+            .Concat(options?.SupportedCultures.Where(culture => culture != options?.DefaultCulture) ?? []) .ToArray();
+    }
+};
+
+builder.Services.AddScoped<IBrandingProvider, StudioBrandingProvider>();
+builder.Services.AddCore().Replace(new(typeof(IBrandingProvider), typeof(StudioBrandingProvider), ServiceLifetime.Scoped));
+builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(options));
+builder.Services.AddRemoteBackend(backendApiConfig);
+builder.Services.AddLoginModule();
+builder.Services.UseElsaIdentity();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
+builder.Services.AddLocalizationModule(localizationConfig);
+builder.Services.AddTranslations();
+
+// Replace some services with other implementations.
+builder.Services.AddScoped<ITimeZoneProvider, LocalTimeZoneProvider>();
+
+// Build the application.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseElsaLocalization();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.Run();
---
+++ src/workbench-studio/Elsa.Studio.Workbench/StudioBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Elsa.Studio.Branding;
+
+namespace Elsa.Studio.Host.Server
+{
+    /// <inheritdoc />
+    public class StudioBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "Elsa Studio 3.6";
+    }
+}
